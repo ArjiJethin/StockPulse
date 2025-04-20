c2dba85ab187309cb6aac7489a1a6fdcaad74ea7
Eswar Arji <arjijethin.ac0705@gmail.com>
2025-04-20 21:21:19 +0530

    Update index.html and style.css for dark mode support

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -9,3 +9,22 @@ registerBtn.addEventListener("click", () => {
 loginBtn.addEventListener("click", () => {
     container.classList.remove("active"); // Remove "active" to switch back to login
 });
+
+// Check localStorage and apply dark mode on page load
+const modeBtn = document.querySelector(".mode-btn");
+
+document.addEventListener("DOMContentLoaded", () => {
+    const darkModeState = localStorage.getItem("darkMode");
+    if (darkModeState === "enabled") {
+        document.body.classList.add("dark-mode");
+    }
+});
+
+function darkMode() {
+    document.body.classList.toggle("dark-mode");
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("darkMode", "enabled");
+    } else {
+        localStorage.setItem("darkMode", "disabled");
+    }
+}
